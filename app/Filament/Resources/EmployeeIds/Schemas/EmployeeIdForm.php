f386fb4b25a9145ac18149c0d9b981dade1fd4b8
Keyven Plariza <keyv3k29@gmail.com>
2026-04-20 13:04:04 +0800

    My commit now

--- app/Filament/Resources/EmployeeIds/Schemas/EmployeeIdForm.php
+++ app/Filament/Resources/EmployeeIds/Schemas/EmployeeIdForm.php
@@ -2,10 +2,10 @@
 
 namespace App\Filament\Resources\EmployeeIds\Schemas;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas;
-use Filament\Schemas\Components\FileUpload;
-use Filament\Schemas\Components\Section;
-use Filament\Schemas\Components\TextInput;
 
 class EmployeeIdForm
 {
